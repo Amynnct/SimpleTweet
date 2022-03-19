09d3cc6afe3951abe4b0f085ccefc51190990285
Amynnct <amei.nguyennct@gmail.com>
2022-03-19 03:00:16 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -0,0 +1,57 @@
+package com.codepath.apps.restclienttemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+class ComposeActivity : AppCompatActivity() {
+    lateinit var etCompose: EditText
+    lateinit var btnTweet: Button
+
+    lateinit var client: TwitterClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose)
+
+        etCompose = findViewById(R.id.etTextTweet)
+        btnTweet = findViewById(R.id.btnTweet)
+
+        client = TwitterApplication.getRestClient(this)
+
+        btnTweet.setOnClickListener {
+            val tweetContent = etCompose.text.toString()
+
+            if (tweetContent.isEmpty()) {
+                Toast.makeText(this, "Empty tweets not allowed!", Toast.LENGTH_SHORT).show()
+            } else
+                if (tweetContent.length > 140) {
+                    Toast.makeText(this, "Tweet is too long! Limit is 140 characters", Toast.LENGTH_SHORT).show()
+                } else {
+                    //Make an api call to Twitter to publish tweet
+                    client.publishTweet(tweetContent, object: JsonHttpResponseHandler() {
+                        override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                            Log.i(TAG, "Successfully published tweet!")
+                        }
+
+                        override fun onFailure(
+                            statusCode: Int,
+                            headers: Headers?,
+                            response: String?,
+                            throwable: Throwable?
+                        ) {
+                            Log.e(TAG, "Failed to publish tweet", throwable)
+                        }
+                    })
+                }
+        }
+    }
+    companion object {
+        val TAG = "ComposeActivity"
+    }
+}
